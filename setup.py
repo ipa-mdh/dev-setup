2bdff3597ce6ca6ae577d2ce487f949d1b9c7985
Max Daiber-Huppert <max.daiber-huppert@ipa.fraunhofer.de>
2025-03-24 11:34:39 +0000

    add argparse; add update routine

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+from loguru import logger
+import argparse
 import yaml
 from pathlib import Path
 import xml.etree.ElementTree as ET
@@ -8,8 +10,46 @@ target_dir = Path(".dev-setup")
 template_dir = Path("dev-setup/template")
 templete_docker_dir = template_dir / "docker"
 
-variables_file = Path(".dev-setup.yml")
+dev_setup_config_file = Path(".dev-setup.yml")
 package_file = Path("package.xml")
+
+def parse_arguments():
+    # Create the argument parser
+    parser = argparse.ArgumentParser(description="Setup script with package and environment settings.")
+    
+    # Add the arguments with default values
+    parser.add_argument(
+        '--package_name', 
+        type=str, 
+        default='new_package', 
+        help='The name of the package (default: new_package)'
+    )
+    
+    parser.add_argument(
+        '--environment', 
+        type=str, 
+        choices=['ros.noetic', 'ros.humble', 'NiceGUI', 'debian'], 
+        default='ros.humble', 
+        help='The environment for the package (default: ros.humble)'
+    )
+    
+    parser.add_argument(
+        '--vnc', 
+        action='store_true', 
+        help='Enable VNC (default: False)'
+    )
+    
+    parser.add_argument(
+        '--yes', '-y',
+        action='store_true',
+        help="Automatically answer 'yes' to all prompts"
+    )
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Return the parsed arguments
+    return args
 
 def render_template(file: Path, variables: dict, targtet: Path):
     # Load the Jinja2 template   
@@ -48,30 +88,98 @@ def get_package_name(package_xml_path: Path):
         raise ValueError("No <name> element found in the package.xml file.")
 
     return name_elem.text.strip()
+
+def set_package_name(xml_file, new_package_name):
+    # Parse the XML file
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
     
+    # Find the <name> tag and update its text to the new package name
+    name_tag = root.find('name')
+    if name_tag is not None:
+        name_tag.text = new_package_name
+    else:
+        logger.error("<name> tag not found in the XML file.")
+        return
+    
+    # Write the updated XML back to the file
+    tree.write(xml_file, encoding="UTF-8", xml_declaration=True)
+    logger.info(f"Package name updated to: {new_package_name}")
+
+def get_dev_setup_template_filename(environment:str):
+    if environment:
+        filename = f".dev-setup.{environment}.yml.j2"
+    else:
+        filename = ".dev-setup.yml.j2"
+    
+    return filename
+    
+def ask_for_update(variable_name, current_value, new_value, auto_yes):
+    """Ask the user whether they want to update the variable. If auto_yes is True, always return the new value."""
+    if auto_yes:
+        return new_value
+    
+    while True:
+        user_input = input(f"Do you want to update '{variable_name}' with '{new_value}' (current value: {current_value})? (y/n): ").strip().lower()
+        if user_input == 'y':
+            return new_value
+        elif user_input == 'n':
+            return current_value
+        else:
+            print("Invalid input. Please answer with 'y' or 'n'.")
 
 def main():
-    default_package_name = "my_package"
-    variables = {}
+    args = parse_arguments()
+    # Display the parsed values
+    logger.info(f"Package Name: {args.package_name}")
+    logger.info(f"Environment: {args.environment}")
+    logger.info(f"VNC Enabled: {args.vnc}")
+    logger.info(f"All yes: {args.yes}")
     
+    # exit()
+    
+    dev_setup_config = {}
+    package_name = args.package_name
+    
+    dev_setup_template_filename = get_dev_setup_template_filename(args.environment)
+    
+    if dev_setup_config_file.exists():
+        logger.info(f"Variables file 'variables_file' found.")
         
-    if not variables_file.exists():
+        # Load the variables file
+        update_config_file = False
+        with open(dev_setup_config_file) as f:
+            dev_setup_config = yaml.safe_load(f)
+            
+            if package_name != "new_package":
+                if dev_setup_config['package_name'] != args.package_name:
+                    logger.warning(f"The package name in the configuration file differs from the dev-setup argument. {dev_setup_config['package_name']} -> {args.package_name }")
+                    dev_setup_config['package_name'] = ask_for_update('package_name', dev_setup_config['package_name'], args.package_name, args.yes)
+                    update_config_file = True
+            
+            if dev_setup_config['environment'] != args.environment:
+                logger.warning("The environment in the configuration file differs from the dev-setup argument.")
+                dev_setup_config['environment'] = ask_for_update('environment', dev_setup_config['environment'], args.environment,args.yes)
+                update_config_file = True
+                
+        if update_config_file:
+            logger.debug(dev_setup_config)
+            render_template(template_dir / dev_setup_template_filename, dev_setup_config, dev_setup_config_file)
+            
+        
+    else:
         if package_file.exists():
             package_name = get_package_name(package_file)
-            variables["package_name"] = package_name
+            dev_setup_config["package_name"] = package_name
         else:
-            variables["package_name"] = "my_package"
-            print("No package.xml file found. Using 'my_package' as the package name.")
-        render_template(template_dir / ".dev-setup.yml.j2", variables, variables_file)
+            dev_setup_config["package_name"] = package_name
+            logger.warning(f"No package.xml file found. Using '{package_name}' as the package name.")
+        render_template(template_dir / dev_setup_template_filename, dev_setup_config, dev_setup_config_file)
 
-    # Load the variables file
-    with open(variables_file) as f:
-        variables = yaml.safe_load(f)
-        if variables['package_name'] == default_package_name:
-            print("Update the package name in .dev-setup.yml and run this script again.")
     
-    render_template_folder(templete_docker_dir, variables, target_dir / "docker")
-    render_template(template_dir / "devcontainer.json.j2", variables, Path(".devcontainer/devcontainer.json"))
+    
+    render_template_folder(templete_docker_dir, dev_setup_config, target_dir / "docker")
+    render_template(template_dir / "devcontainer.json.j2", dev_setup_config, Path(".devcontainer/devcontainer.json"))
 
 if __name__ == "__main__":
     main()
