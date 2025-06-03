a309015bf2e9cc629aedfb374713df14dae32fcc
mdh <max.daiber-huppert@ipa.fraunhofer.de>
2025-06-03 12:53:40 +0000

    Enhance development setup scripts and templates
    - Updated setup.py to improve template rendering and path handling.
    - Added functions to remove variables with curly braces and update nested dictionaries.
    - Enhanced YAML configuration loading with support for default values and nested updates.
    - Modified template files to use 'args' for variable access, ensuring consistency across templates.
    - Introduced new Dockerfile templates for base, CI, devcontainer, and run configurations.
    - Created build, entrypoint, and push scripts for Docker image management.
    - Implemented installation scripts for dependencies and Git credentials setup.
    - Added a GitLab CI configuration template to streamline CI/CD processes.

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
+
+# Executable script for setting up a development environment with ROS packages and configurations.
+
 from loguru import logger
 import argparse
 import yaml
 from pathlib import Path
 import xml.etree.ElementTree as ET
-from jinja2 import Environment, FileSystemLoader
+from jinja2 import Environment, FileSystemLoader, Template
+import shutil
 
 target_dir = Path(".dev-setup")
 template_dir = Path("dev-setup/template")
-templete_docker_dir = template_dir / "docker"
-templete_ci_cd_dir = template_dir / "ci-cd"
+templete_project_root_dir = template_dir / "project_root"
 
 dev_setup_config_file = Path(".dev-setup.yml")
 package_file = Path("package.xml")
@@ -61,7 +64,7 @@ def render_template(file: Path, variables: dict, targtet: Path):
     template = env.get_template(file.name)
 
     # Render the Dockerfile
-    output = template.render(variables)
+    output = template.render(args=variables)
 
     # Create the target directory if it doesn't exist
     if not targtet.parent.exists():
@@ -71,12 +74,49 @@ def render_template(file: Path, variables: dict, targtet: Path):
     with open(targtet, "w") as f:
         f.write(output)
 
-def render_template_folder(template_dir, variables, output_dir):
-    # get template files
-    template_files = list(template_dir.glob("*.j2"))
+def render_path(path: Path, context: dict) -> Path:
+    """Render each part of the path as a Jinja2 template."""
+    parts = []
+    for part in path.parts:
+        rendered = Template(part).render(args=context)
+        parts.append(rendered)
+        
+    logger.debug(f"Rendering path: {path} -> {parts}")
+    return Path(*parts)
 
-    for file in template_files:
-        render_template(file, variables, output_dir / file.with_suffix("").name)
+def render_template_folder(template_dir, context, output_dir):
+    # get template files
+    # template_files = list(template_dir.glob("*.j2"))
+
+    # for file in template_files:
+    #     render_template(file, variables, output_dir / file.with_suffix("").name)
+    
+    for file in template_dir.rglob("*"):
+        logger.info(f"Processing file: {file}")
+        
+        rel_path = file.relative_to(template_dir)
+        dest_path = output_dir / render_path(rel_path, context)
+        logger.debug(f"Destination path: {dest_path}")
+
+        if dest_path.is_dir():
+            dest_path.mkdir(parents=True, exist_ok=True)
+            
+        elif file.suffix == ".j2":
+            with open(file) as f:
+                template = Template(f.read())
+                rendered = template.render(args=context)
+            dest_path = dest_path.with_suffix("")  # Remove .j2 extension
+            # create parent directories if they don't exist
+            dest_path.parent.mkdir(parents=True, exist_ok=True)
+            with open(dest_path, "w") as f:
+                f.write(rendered)
+        else:
+            if file.is_file():
+                logger.debug(f"Copying file: {file} to {dest_path}")
+                # create parent directories if they don't exist
+                dest_path.parent.mkdir(parents=True, exist_ok=True)
+                shutil.copy(src=file, dst=dest_path)
+
 
 def get_package_name(package_xml_path: Path):
     """Extracts the package name from a package.xml file."""
@@ -124,13 +164,62 @@ def ask_for_update(variable_name, current_value, new_value, auto_yes):
         return new_value
     
     while True:
-        user_input = input(f"Do you want to update '{variable_name}' with '{new_value}' (current value: {current_value})? (y/n): ").strip().lower()
-        if user_input == 'y':
-            return new_value
-        elif user_input == 'n':
-            return current_value
+        if current_value != new_value:
+            user_input = input(f"Do you want to update '{variable_name}' with '{new_value}' (current value: {current_value})? (y/n): ").strip().lower()
+            if user_input == 'y':
+                return new_value
+            elif user_input == 'n':
+                return current_value
+            else:
+                print("Invalid input. Please answer with 'y' or 'n'.")
         else:
-            print("Invalid input. Please answer with 'y' or 'n'.")
+            return current_value  # No change needed, return current value
+
+def remove_variables_with_curly_braces(d):
+    """Recursively remove variables from a dictionary that contains '{{'."""
+    buffer = {}
+    for k, v in d.items():
+        if isinstance(v, dict):
+            buffer[k] = remove_variables_with_curly_braces(v)
+        elif isinstance(v, str) and '{{' in v:
+            logger.debug(f"Removing variable '{k}' with value '{v}' containing '{{'")
+        else:
+            buffer[k] = v
+    return buffer
+
+def update_nested_dict_with_dict(original_dict: dict, updates: dict) -> dict:
+    """Update a nested dictionary with another dictionary."""
+    for key, value in updates.items():
+        if isinstance(value, dict) and key in original_dict and isinstance(original_dict[key], dict):
+            update_nested_dict_with_dict(original_dict[key], value)
+        else:
+            original_dict[key] = value
+    return original_dict
+
+def get_dev_setup_config(file: Path, dev_setup_template_file: Path):
+    """Load the dev-setup configuration from a YAML file."""
+    update = True
+    if not dev_setup_template_file.exists():
+        raise FileNotFoundError(f"The template file {dev_setup_template_file} does not exist.")
+    if not file.exists():
+        update = False
+        logger.info(f"The configuration file {file} does not exist. Using only the template file {dev_setup_template_file}.")
+    
+    with open(dev_setup_template_file) as f:
+        dev_setup_config_base = yaml.safe_load(f)
+        dev_setup_config_base = remove_variables_with_curly_braces(dev_setup_config_base)
+    
+    if update:
+        with open(file, 'r') as f:
+            custom_config = yaml.safe_load(f)
+        
+        if custom_config is None:
+            raise ValueError("The configuration file is empty or invalid.")
+        config = update_nested_dict_with_dict(dev_setup_config_base.copy(), custom_config)
+    else:
+        config = dev_setup_config_base
+    
+    return config
 
 def main():
     args = parse_arguments()
@@ -147,40 +236,43 @@ def main():
     
     dev_setup_template_filename = get_dev_setup_template_filename(args.environment)
     
+    dev_setup_config = get_dev_setup_config(dev_setup_config_file, template_dir / dev_setup_template_filename)
+    
     if dev_setup_config_file.exists():
         logger.info(f"Variables file 'variables_file' found.")
         
         # Load the variables file
-        with open(dev_setup_config_file) as f:
-            dev_setup_config = yaml.safe_load(f)
+        # with open(dev_setup_config_file) as f:
+        #     dev_setup_config = yaml.safe_load(f)
             
-            if package_name != "new_package":
-                # package_name is not the default
-                if dev_setup_config['package_name'] != args.package_name:
-                    logger.warning(f"The package name in the configuration file differs from the dev-setup argument. {dev_setup_config['package_name']} -> {args.package_name }")
-                    dev_setup_config['package_name'] = ask_for_update('package_name',
-                                                                      dev_setup_config['package_name'],
-                                                                      args.package_name,
-                                                                      args.yes)
-            
-            if args.environment != '':
-                if dev_setup_config['environment'] != args.environment:
-                    logger.warning("The environment in the configuration file differs from the dev-setup argument.")
-                    dev_setup_config['environment'] = ask_for_update('environment',
-                                                                    dev_setup_config['environment'],
-                                                                    args.environment,
+        if package_name != "new_package":
+            # package_name is not the default
+            if dev_setup_config['package_name'] != args.package_name:
+                logger.warning(f"The package name in the configuration file differs from the dev-setup argument. {dev_setup_config['package_name']} -> {args.package_name }")
+                dev_setup_config['package_name'] = ask_for_update('package_name',
+                                                                    dev_setup_config['package_name'],
+                                                                    args.package_name,
                                                                     args.yes)
-            
-            if dev_setup_config['devcontainer']['feature']['desktop_lite'] != args.desktop_lite:
-                logger.warning("The desktop_lite feature in the configuration file differs from the dev-setup argument.")
-                dev_setup_config['devcontainer']['feature']['desktop_lite'] = ask_for_update('desktop_lite',
-                                                                                             dev_setup_config['devcontainer']['feature']['desktop_lite'],
-                                                                                             args.desktop_lite,
-                                                                                             args.yes)
+        
+        if args.environment != '':
+            if dev_setup_config['environment'] != args.environment:
+                logger.warning("The environment in the configuration file differs from the dev-setup argument.")
+                dev_setup_config['environment'] = ask_for_update('environment',
+                                                                dev_setup_config['environment'],
+                                                                args.environment,
+                                                                args.yes)
+        
+        if dev_setup_config['devcontainer']['feature']['desktop_lite'] != args.desktop_lite:
+            logger.warning("The desktop_lite feature in the configuration file differs from the dev-setup argument.")
+            dev_setup_config['devcontainer']['feature']['desktop_lite'] = ask_for_update('desktop_lite',
+                                                                                            dev_setup_config['devcontainer']['feature']['desktop_lite'],
+                                                                                            args.desktop_lite,
+                                                                                            args.yes)
         
     else:
-        with open(template_dir / dev_setup_template_filename) as f:
-            dev_setup_config = yaml.safe_load(f)
+        # with open(template_dir / dev_setup_template_filename) as f:
+        #     dev_setup_config = yaml.safe_load(f)
+        #     dev_setup_config = remove_variables_with_curly_braces(dev_setup_config)
             
         if args.environment != '':
             dev_setup_config['environment'] = args.environment
@@ -203,9 +295,7 @@ def main():
     with open(dev_setup_config_file) as f:
         dev_setup_config = yaml.safe_load(f)
     
-    render_template_folder(templete_docker_dir, dev_setup_config, target_dir / "docker")
-    render_template_folder(templete_ci_cd_dir, dev_setup_config, target_dir.parent)
-    render_template(template_dir / "devcontainer.json.j2", dev_setup_config, Path(".devcontainer/devcontainer.json"))
+    render_template_folder(templete_project_root_dir, dev_setup_config, target_dir.parent)
 
 if __name__ == "__main__":
     main()
